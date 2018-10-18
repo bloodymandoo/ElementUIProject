c492d32b071d19ee9de2bbb6bde54dcb51e3ad65
bloodymandoo <1003711565@qq.com>
2018-10-18 23:41:38 +0800

    add element-ui reset.css global.scss

--- elementUIProject/src/App.vue
+++ elementUIProject/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-main><router-view/></el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -11,13 +13,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height:100%;
+  .el-container{
+    height:100%;
+  }
 }
 </style>
